4f72ab74fce31f2dd7beef734589d988fa36717d
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2019-06-05 01:13:51 +0700

    Create structure

--- public/electron.js
+++ public/electron.js
@@ -1,9 +1,9 @@
-const electron = require('electron');
+const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
-const path = require('path');
-const isDev = require('electron-is-dev');
+const path = require("path");
+const isDev = require("electron-is-dev");
 
 let MainWindow;
 
@@ -11,27 +11,28 @@ function createWindow() {
   MainWindow = new BrowserWindow({
     width: 900,
     height: 680,
+    title: "Hitrans"
   });
   MainWindow.loadURL(
     isDev
-      ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`,
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
   );
-  if (isDev) {
-    MainWindow.webContents.openDevTools();
-  }
-  MainWindow.on('closed', () => (MainWindow = null));
+  MainWindow.on("closed", () => (MainWindow = null));
+  MainWindow.on("page-title-updated", function(e) {
+    e.preventDefault();
+  });
 }
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (MainWindow === null) {
     createWindow();
   }
