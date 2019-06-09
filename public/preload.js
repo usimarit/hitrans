1ed3a716cb2dd404efa8e99d51e0aadd387f71dc
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2019-06-09 17:46:38 +0700

    Update

--- public/preload.js
+++ public/preload.js
@@ -2,8 +2,10 @@ const electron = require('electron').remote;
 const dialog = electron.dialog;
 const BrowserView = electron.BrowserView;
 const BrowserWindow = electron.BrowserWindow;
+const zerorpc = require('zerorpc');
 
 window.electron = {};
 window.electron.dialog = dialog;
 window.electron.BrowserView = BrowserView;
 window.electron.BrowserWindow = BrowserWindow;
+window.electron.zerorpc = zerorpc;
--- src/components/popup.js
+++ src/components/popup.js
@@ -6,24 +6,24 @@ function popUp() {
   client.invoke('getMousePosition', (error, result) => {
     if (error) {
       console.error(error);
-    } else {
-      x = result[0];
-      y = result[1];
+      return;
     }
-  });
-  let framelessWin = new BrowserWindow({
-    x: x,
-    y: y,
-    width: 400,
-    height: 500,
-    frame: false,
-    alwaysOnTop: true,
-    resizable: false,
-  });
+    x = result[0];
+    y = result[1];
+    let framelessWin = new BrowserWindow({
+      x: x,
+      y: y,
+      width: 400,
+      height: 500,
+      frame: false,
+      alwaysOnTop: true,
+      resizable: false,
+    });
 
-  framelessWin.setMenuBarVisibility(false);
-  framelessWin.on('blur', () => framelessWin.close());
-  framelessWin.show();
+    framelessWin.setMenuBarVisibility(false);
+    framelessWin.on('blur', () => framelessWin.close());
+    framelessWin.show();
+  });
 }
 
 export default popUp;
