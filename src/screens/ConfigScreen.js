655d53d0b241a008d91b31c09318d45c6c9247d2
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2019-06-12 23:11:30 +0700

    Add settings

--- src/screens/ConfigScreen.js
+++ src/screens/ConfigScreen.js
@@ -1,9 +1,76 @@
 import React from 'react';
 import ScreenWrapper from '../components/general/ScreenWrapper';
+import SettingItem from '../components/settings/item';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import '../css/settings/settings.css';
+
+const lang = [
+  {
+    name: 'English',
+    value: 'en',
+  },
+];
 
 class ConfigScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      source_lang: '',
+      target_lang: '',
+    };
+  }
+  changeLang = e => {
+    this.setState({ ...this.state, [e.target.name]: e.target.value });
+  };
   render() {
-    return <ScreenWrapper></ScreenWrapper>;
+    return (
+      <ScreenWrapper>
+        <SettingItem title="Languages">
+          <form className="languages" autoComplete="off">
+            <FormControl
+              className="languages_item"
+              style={{ marginLeft: 0 }}
+              variant="outlined">
+              <InputLabel htmlFor="source_lang">Source</InputLabel>
+              <Select
+                value={this.state.source_lang}
+                onChange={this.changeLang}
+                input={<OutlinedInput name="source_lang" id="source_lang" />}>
+                {lang.map((item, index) => {
+                  return (
+                    <MenuItem key={index} value={item.value}>
+                      {item.name}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+            </FormControl>
+            <FormControl
+              className="languages_item"
+              style={{ marginRight: 0 }}
+              variant="outlined">
+              <InputLabel htmlFor="target_lang">Target</InputLabel>
+              <Select
+                value={this.state.target_lang}
+                onChange={this.changeLang}
+                input={<OutlinedInput name="target_lang" id="target_lang" />}>
+                {lang.map((item, index) => {
+                  return <MenuItem value={item.value}>{item.name}</MenuItem>;
+                })}
+              </Select>
+            </FormControl>
+          </form>
+        </SettingItem>
+        <SettingItem title="Google API Key" init_hidden={true}></SettingItem>
+        <SettingItem
+          title="Google Translation Engine"
+          init_hidden={true}></SettingItem>
+      </ScreenWrapper>
+    );
   }
 }
 
--- src/screens/SettingScreen.js
+++ src/screens/SettingScreen.js
@@ -5,31 +5,90 @@ import Checkbox from '@material-ui/core/Checkbox';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
+import Button from '@material-ui/core/Button';
+import '../css/settings/settings.css';
 
 class SettingScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      double_click: false, // Load settings
+      highlight: false, // Load settings
+      is_current_shortcut: false, // Load settings
+      current_shortcut: 'No value set',
+      is_pressing_keys: false,
+    };
+  }
+  selectionChanged = name => e => {
+    this.setState({ ...this.state, [name]: e.target.checked });
+  };
+  checkExistingKeyComb = (key_comb, callback) => {
+    callback();
+  };
+  handleInputCombination = callback => {
+    this.checkExistingKeyComb([], callback);
+  };
+  changeShortcut = () => {
+    this.setState({
+      current_shortcut: 'Press key combinations',
+      is_pressing_keys: true,
+    });
+    // Handle input combinations
+    this.handleInputCombination(() => {
+      this.setState({ is_pressing_keys: false });
+    });
+  };
   render() {
     return (
       <ScreenWrapper>
         <SettingItem title="Translation on text selection">
           <FormControl component="fieldset">
-            <FormGroup onChange={this.selectionChanged} column>
+            <FormGroup>
               <FormControlLabel
-                value="double_click"
-                control={<Checkbox color="primary" />}
+                control={
+                  <Checkbox
+                    value="double_click"
+                    color="primary"
+                    onChange={this.selectionChanged('double_click')}
+                    checked={this.state.double_click}
+                  />
+                }
                 label="On double click"
-                labelPlacement="end"
               />
               <FormControlLabel
                 value="highlight"
-                control={<Checkbox color="primary" />}
+                control={
+                  <Checkbox
+                    color="primary"
+                    onChange={this.selectionChanged('highlight')}
+                    checked={this.state.highlight}
+                  />
+                }
                 label="On finished selection"
-                labelPlacement="end"
               />
             </FormGroup>
           </FormControl>
         </SettingItem>
-        <SettingItem title="Shortcuts"></SettingItem>
+        <SettingItem title="Shortcuts">
+          <FormControl component="fieldset">
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Button
+                    variant="contained"
+                    className="shortcut"
+                    disabled={this.state.is_pressing_keys}
+                    onClick={this.changeShortcut}
+                    color={this.state.is_current_shortcut ? 'primary' : null}>
+                    {this.state.current_shortcut}
+                  </Button>
+                }
+                label="Click to change keyboard shortcut"
+                labelPlacement="start"
+              />
+            </FormGroup>
+          </FormControl>
+        </SettingItem>
       </ScreenWrapper>
     );
   }
