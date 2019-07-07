5134e9e4e430f478c5110fcbf17240ab905927e5
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2019-07-07 15:21:49 +0700

    Make it work a little bit

--- rpc/client.py
+++ rpc/client.py
@@ -8,24 +8,18 @@ import secondrpc_pb2_grpc
 
 
 def client():
-    with grpc.insecure_channel('localhost:1235') as channel:
+    def on_click(x, y, button, pressed):
+        channel = grpc.insecure_channel('localhost:1235')
         stub = secondrpc_pb2_grpc.SecondRpcStub(channel)
-
-        def on_click(x, y, button, pressed):
-            if not pressed:
-                text = get_selected_text()
-                if text == '':
-                    return
-                print(text)
-                stub.PopUp(secondrpc_pb2.PopData(
-                    text=text,
-                    x=x,
-                    y=y
-                ))
-
-        print("client starting at port 1235")
-        with mouse.Listener(on_click=on_click) as listener:
-            listener.join()
+        if not pressed:
+            text = get_selected_text()
+            if text == '':
+                return
+            print(text)
+            stub.PopUp(secondrpc_pb2.PopData(text=text, x=x, y=y))
+    print("Client started to listen port 1235")
+    with mouse.Listener(on_click=on_click) as listener:
+        listener.join()
 
 
 if __name__ == '__main__':
